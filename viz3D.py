44401d856eb76dd416300e5ef4fe06eb0592029e
XIAOAO S <45730495+lineojcd@users.noreply.github.com>
2023-05-08 17:13:07 -0700

    Update viz3D.py

--- viz3D.py
+++ viz3D.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) [2022] [Luc Jaulin]
+# Copyright (c) [2022] [Luc Jaulin] 
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
